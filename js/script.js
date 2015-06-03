5d9a9bf75cded018a81a478d1e964e64f932dcd4
Ashraful Prium <prium47@gmail.com>
2015-06-03 18:17:40 +0600

    Isotope Updated

--- js/script.js
+++ js/script.js
@@ -40,16 +40,6 @@ $(document).ready(function() {
 	percentWeight: 'normal'
 	});
 
-	var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
 
 /*=================================
 ||			Owl Carousel
@@ -95,6 +85,7 @@ $(document).ready(function() {
 	// init Isotope
 	var $container = $('#works_container').isotope({
 	  // options
+      itemSelector: '.works-single-item'
 	});
 	// filter items on button click
 	$('#filters').on( 'click', 'button', function() {
